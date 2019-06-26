84b79cda7dc735affdde3f8c8b116801598f6bd5
Dion Lopes <dion.mello@hotmail.com>
2019-06-26 20:00:10 -0300

    Modulo1 Feito

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,8 +21,9 @@ module.exports = {
   rules: {
 		"prettier/prettier": "error",
     "class-methods-use-this": "off",
-    "no-param-reasign": "off",
-    "camelcase": "off",
+		"no-param-reassign": "off",
+		"camelcase": "off",
+		'no-console': 'off',
     "no-unused-vars": ["error", { "argsIgnorePattern": "next"}],
   },
 };
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+	secret: '1bd3e88d0eea66dc6fcb855caa377535',
+	expireIn: '7d',
+};
