df6813a3251a3952873ae7bd40d8993e4440f987
Dion Lopes <dion.mello@hotmail.com>
2019-06-27 22:33:42 -0300

    Backend Semi finalizado

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: '2525',
+	secure: false,
+	auth: {
+		user: '29cf4a8fab56d8',
+		pass: '274fd0ba9c3632',
+	},
+	default: {
+		from: 'Equipe GoBarber <noreply@gobarber.com>',
+	},
+};
